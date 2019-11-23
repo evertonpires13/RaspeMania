57690ae65c262c2de83da1148170f3fd17267368
evertonpires <everton@solucao3.com>
2019-11-23 16:41:52 -0300

    teste

--- app/src/main/java/br/com/freelas/app/mobile/raspe/mania/raspemania/view/activity/MainActivity.java
+++ app/src/main/java/br/com/freelas/app/mobile/raspe/mania/raspemania/view/activity/MainActivity.java
@@ -1,35 +1,16 @@
 package br.com.freelas.app.mobile.raspe.mania.raspemania.view.activity;
 
+import android.os.Bundle;
+
 import br.com.freelas.app.mobile.raspe.mania.raspemania.R;
 import br.com.freelas.app.mobile.raspe.mania.raspemania.base.BaseActivity;
-import br.com.freelas.app.mobile.raspe.mania.raspemania.firebase.GenericInterface;
 
-import android.os.Bundle;
-import android.widget.Toast;
-
-public class MainActivity extends BaseActivity implements GenericInterface {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-
-
-    @Override
-    public void sucessWindow(String node) {
-        Toast.makeText(this, "sucesso", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void sucessList(String node) {
-        Toast.makeText(this, "sucesso lista", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void error(String mensagem) {
-        Toast.makeText(this, "erro ", Toast.LENGTH_SHORT).show();
     }
 
 
