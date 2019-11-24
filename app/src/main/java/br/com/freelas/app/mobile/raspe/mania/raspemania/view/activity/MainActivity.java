010b18255e150282e453dd6685a0e6b99138ff3a
daniele guimarães <daniele.guimaraes@solutis.com.br>
2019-11-24 19:13:47 -0300

    no message

--- app/src/main/java/br/com/freelas/app/mobile/raspe/mania/raspemania/view/activity/MainActivity.java
+++ app/src/main/java/br/com/freelas/app/mobile/raspe/mania/raspemania/view/activity/MainActivity.java
@@ -1,23 +1,34 @@
 package br.com.freelas.app.mobile.raspe.mania.raspemania.view.activity;
 
 import br.com.freelas.app.mobile.raspe.mania.raspemania.R;
-import br.com.freelas.app.mobile.raspe.mania.raspemania.base.BaseActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 
 
-import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends BaseActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AppCompatButton rota = findViewById(R.id.example_btn);
+       /*AppCompatButton rota = findViewById(R.id.example_btn);
 
         rota.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,27 +58,44 @@ public class MainActivity extends BaseActivity {
                 startActivity(intent);
             }
         });
+*/
 
-        AppCompatButton produto_btn = findViewById(R.id.produto_btn);
-        produto_btn.setOnClickListener(new View.OnClickListener() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getBaseContext(), ProdutoActivity.class);
-                startActivity(intent);
+                Snackbar.make(view, "Nova leitura", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
-
-        AppCompatButton local_btn = findViewById(R.id.local_btn);
-        local_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getBaseContext(), LocalActivity.class);
-                startActivity(intent);
-            }
-        });
-
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_colaborador, R.id.nav_estabelecimento,
+                R.id.nav_leitura, R.id.nav_relatorio, R.id.nav_rota, R.id.nav_produto, R.id.nav_example)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
 }
